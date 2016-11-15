262ecca989504abc8a5f536eb57ffcec921544ec
reckbo <reckbo@bwh.harvard.edu>
2016-11-15 16:22:00 +1100

    add AtlasPair

--- config/Pipeline.hs
+++ config/Pipeline.hs
@@ -4,7 +4,7 @@ import           Data.List                (intercalate)
 import           Development.Shake.Config
 import           OutputDirectory          (outdir)
 import           Shake.BuildKey
-import           System.Directory         as IO
+import           qualified System.Directory         as IO
 -- import           Software.UKFTractography (UKFTractographyExe (..), rules)
 import           AntsPath                 (antsPath, antsSrc)
 import qualified Intrust
@@ -12,40 +12,63 @@ import qualified Intrust
 
 type CaseId = String
 
-data ImageType = Bse | Mask
+data ImageType = Bse | TensorMask
         deriving (Show,Generic,Typeable,Eq,Hashable,Binary,NFData,Read)
 
-data ImageFormat = Nrrd | Nifti
+data MaskingAlgorithm = NNLSFusion
+                      | MABS
         deriving (Show,Generic,Typeable,Eq,Hashable,Binary,NFData,Read)
 
-extFromFormat Nrrd = "nrrd"
-extFromFormat Nifti = "nii.gz"
-
-data BseAtlas = BseAtlas CaseId CaseId
+newtype TrainingMask = TrainingMask CaseId
         deriving (Show,Generic,Typeable,Eq,Hashable,Binary,NFData,Read)
 
-instance BuildKey BseAtlas where
-  paths x = [atlasPath Bse x, atlasPath Mask x]
-    where
-      atlasPath imgtype (BseAtlas caseid caseid')
-        = outdir
-        </> "atlases"
-        </> (intercalate "-" [show imgtype, caseid, "in", caseid']) <.> "nii.gz"
+toNii nrrd out = unit $ cmd "ConvertBetweenFileFormats" nrrd out
 
-  build out@(BseAtlas caseid caseidT) = Just $ withTempDir $ \tmpdir -> do
+instance BuildKey TrainingMask where
+  path (TrainingMask caseid) = outdir </> caseid </> (caseid ++"-tensor-mask")
+    <.> "nii.gz"
+
+  build out@(TrainingMask caseid) = Just $ withTempFile $ \tmpnrrd -> do
+    let mask = Intrust.path "dwimask" caseid
+    need [mask]
+    unit $ cmd "center.py -i" mask "-o" tmpnrrd
+    toNii tmpnrrd (path out)
+
+newtype TrainingBse = TrainingBse CaseId
+        deriving (Show,Generic,Typeable,Eq,Hashable,Binary,NFData,Read)
+
+instance BuildKey TrainingBse where
+  path (TrainingBse caseid) = outdir </> caseid </> (caseid ++ "-bse") <.> "nii.gz"
+
+  build out@(TrainingBse caseid) = Just $ withTempFile $ \tmpf -> do
+    let tmpnrrd = tmpf <.> "nrrd"
     let dwied = Intrust.path "dwied" caseid
-        dwimask = Intrust.path "dwimask" caseid
-        dwiedT = Intrust.path "dwied" caseidT
-    need [dwied, dwiedT, dwimask, replaceExtension dwimask "raw.gz"]
-    let bse = tmpdir </> "bse.nrrd"
-        bseT = tmpdir </> "bseT.nrrd"
-    unit $ cmd "bse.sh -i" dwied "-o" bse
-    unit $ cmd "bse.sh -i" dwiedT "-o" bseT
+    need [dwied, replaceExtension dwied "raw.gz"]
+    unit $ cmd "bse.sh -i" dwied "-o" tmpnrrd
+    unit $ cmd "center.py -i" tmpnrrd "-o" tmpnrrd
+    toNii tmpnrrd (path out)
+
+
+data AtlasPair = AtlasPair CaseId CaseId
+        deriving (Show,Generic,Typeable,Eq,Hashable,Binary,NFData,Read)
+
+instance BuildKey AtlasPair where
+  paths x = [atlasPath Bse x, atlasPath TensorMask x]
+    where
+      atlasPath imgtype (AtlasPair caseidT caseid)
+        = outdir </> caseidT
+        </> (intercalate "-" [show imgtype, caseid, "in", caseidT]) <.> "nii.gz"
+
+  build out@(AtlasPair caseidT caseid) = Just $ withTempDir $ \tmpdir -> do
+    let bse = TrainingBse caseid
+        bseT = TrainingBse caseidT
+    apply1 (TrainingMask caseid) :: Action [Double]
+    apply [bse, bseT] :: Action [[Double]]
     let pre = "bse_to_target"
         bseWarped = pre ++ "Warped.nii.gz"
     command_ [] (antsSrc </> "Scripts/antsRegistrationSyN.sh") ["-d", "3"
-                                                              ,"-f", bseT
-                                                              ,"-m", bse
+                                                              ,"-f", path bseT
+                                                              ,"-m", path bse
                                                               ,"-o", pre
                                                               ,"-n", "16"]
     let xfmRigid = pre ++ "0GenericAffine.mat"
@@ -53,15 +76,15 @@ instance BuildKey BseAtlas where
         xfm = pre ++ "Transform.nii.gz"
     command_ [] (antsPath </> "ComposeMultiTransform") ["3"
                                                       ,xfm
-                                                      ,"-R", bseT
+                                                      ,"-R", path bseT
                                                       ,xfmWarp
                                                       ,xfmRigid]
 
     let [bseOut, maskOut] = paths out
     command_ [] (antsPath </> "antsApplyTransforms") ["-d", "3"
-                                                    ,"-i", dwimask
+                                                    ,"-i", path (TrainingMask caseid)
                                                     ,"-o", maskOut
-                                                    ,"-r", bseT
+                                                    ,"-r", path bseT
                                                     ,"-t", xfm]
     liftIO $ IO.renameFile bseWarped bseOut
 
@@ -72,8 +95,12 @@ main = shakeArgs shakeOptions{shakeFiles=outdir, shakeVerbosity=Chatty} $ do
 
   action $ do
     -- Just caseids <- fmap words <$> getConfig "caselist"
-    let caseid = "003_GNX_007"
-        caseidT = "016_NA3_015"
-    apply [BseAtlas caseid caseidT] :: Action [[Double]]
+    Just targetCases <- fmap words <$> getConfig "targetCases"
+    Just trainingCases <- fmap words <$> getConfig "trainingCases"
+    apply [ AtlasPair caseidT caseid
+          | caseidT <- targetCases
+          , caseid <- trainingCases ] :: Action [[Double]]
 
-  rule $ (buildKey :: BseAtlas -> Maybe (Action [Double]))
+  rule $ (buildKey :: AtlasPair -> Maybe (Action [Double]))
+  rule $ (buildKey :: TrainingBse -> Maybe (Action [Double]))
+  rule $ (buildKey :: TrainingMask -> Maybe (Action [Double]))
