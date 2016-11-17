96488eb7bc174a76bf574799c07cb3957b895284
reckbo <reckbo@bwh.harvard.edu>
2016-11-17 15:12:54 +1100

    STY: change R to use data.table to work on cluster's old R

--- config/summarize.R
+++ config/summarize.R
@@ -1,10 +1,8 @@
 #!/usr/bin/env Rscript
 
-require(dplyr)
-
-## args <- commandArgs(trailingOnly = TRUE)
-## d <- read.csv(args[1])
-d <- read.csv("_data/dicecoefficients.csv")
-d.summary <- group_by(d, algo, thresh) %>% summarize(mean=mean(coeff), sd=sd(coeff))
+require(data.table)
+d <- fread('_data/dicecoefficients.csv')
+setkey(d, algo, thresh)
+d.summary <- d[,list(mean=mean(coeff),sd=sd(coeff)),by=list(algo,thresh)]
 write.csv(d.summary, "_data/summary.csv", row.names=F)
 write("Made _data/summary.csv", stdout())
